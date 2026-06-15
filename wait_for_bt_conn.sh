94603cdd95cdc730a3c3ee8c18deb8becca78af1
Miro Meklord <Miro.Meklord@gmail.com>
2026-06-15 15:08:31 +0800

    wait_for_bt_conn.sh: Script linted.

--- wait_for_bt_conn.sh
+++ wait_for_bt_conn.sh
@@ -27,7 +27,8 @@ fi
 # eavesdropping."
 # `busctl monitor` has yet to support the eavesdropping fallback behavior.
 # See: https://github.com/systemd/systemd/issues/26310#issuecomment-1685170189
-dbus-monitor --system "type='signal',sender='org.bluez',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'" 2> /dev/null | \
+watch_expression="type='signal',sender='org.bluez',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'"
+dbus-monitor --system "$watch_expression" 2> /dev/null | \
 while read -r line; do
     if [[ "$line" =~ path=([^;]+) ]]; then
         device_path="${BASH_REMATCH[1]}"
@@ -37,13 +38,15 @@ while read -r line; do
         read -r val_line
         if [[ "$val_line" == *"boolean true"* ]]; then
             if [[ -n $name ]]; then
-                device_name="$(busctl --system get-property org.bluez "$device_path" org.bluez.Device1 Name)"
+                device_name="$(busctl --system get-property org.bluez \
+                    "$device_path" org.bluez.Device1 Name)"
                 if [[ ! $device_name =~ $name_pattern ]]; then
                     continue
                 fi
             fi
             if [[ -n $address ]]; then
-                device_address="$(busctl --system get-property org.bluez "$device_path" org.bluez.Device1 Address)"
+                device_address="$(busctl --system get-property org.bluez \
+                    "$device_path" org.bluez.Device1 Address)"
                 if [[ ! $device_address =~ $address_pattern ]]; then
                     continue
                 fi
